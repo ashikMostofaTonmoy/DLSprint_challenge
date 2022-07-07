014ad143784c682214f35f81633fcac68a02f9dd
ashikMostofaTonmoy <tonmoy.ashik@gmail.com>
2022-07-07 15:58:42 +0600

    added starter code

--- scripst/code_test.py
+++ scripst/code_test.py
@@ -7,3 +7,9 @@ print(sys.path)
 # %%
 # data = os.listdir('../data')
 # print(data)
+
+from datasets import list_datasets, load_dataset, list_metrics, load_metric
+
+# Print all the available datasets
+print(list_datasets())
+# %%
